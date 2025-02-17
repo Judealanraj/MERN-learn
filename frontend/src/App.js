761866d624381865fcfac54abe159925c841e43c
Jude Alan <judealanraj1@gmail.com>
2025-02-17 17:00:17 +0530

    frontend packages install and checked

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import { FaCirclePlus } from "react-icons/fa6";
+
+
+function App() {
+  return (
+  <>
+  <Row>
+    <Col>
+    <h1 className="display-6 text-center">CRUD Operations</h1>
+    <h1><FaCirclePlus></FaCirclePlus></h1>
+    </Col>
+  </Row>
+  </>
+)};
+
+export default App;
