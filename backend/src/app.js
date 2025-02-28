b62ea10b303bbce674de39e339dbecb0ef04c683
Jude Alan <judealanraj1@gmail.com>
2025-02-28 10:55:06 +0530

    Swagger document/ code created for API specification

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,12 @@ import connectDB from "./dataBaseConnect.js";
 import Post from "./model/postmodel.js";
 import router from "./routes/postroutes.js";
 
+import YAML from "yamljs";
+import swagger from "swagger-ui-express"
+
+const yamlFilePath = "./src/api.yaml"
+const swaggerDocument = YAML.load(yamlFilePath)
+
 dotenv.config();
 connectDB();
 const app = express();
@@ -14,6 +20,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/api",router);
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerDocument))
 
 // Basic Route
 app.get("/", (req, res) => {
